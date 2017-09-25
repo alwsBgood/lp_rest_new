dfb86a5af80d99a9b11b1c222ccfcca93cacc1d5
alwsbgood <dima.chebotaru@gmail.com>
2017-09-25 18:26:13 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -104,15 +104,20 @@ function initFormHandler() {
 
 // Mobile menu events SATRT
 function initMobileMenu() {
+  var currentPosition = 0;
 
   $('.account-dropdown-btn').click(function() {
+    currentPosition =  $(window).scrollTop();
     $('.nav-mob').toggleClass('open');
-    $('body').addClass('unscroll')
+    $('body').addClass('unscroll').css({
+      top: -currentPosition
+    });
   })
 
   $('.nav-mob .my-dropdown-menu-overlay, .nav-mob .cross-btn').click(function() {
     $('.nav-mob').removeClass('open');
-    $('body').removeClass('unscroll')
+    $('body').removeClass('unscroll');
+    $(window).scrollTop(currentPosition);
   })
 
   $('.for-business-dropdown, .for-restaurants-dropdown').click(function() {
